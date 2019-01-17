4948adba13876cfdbe978d9e5f1c0720427e8539
otsha <32258613+otsha@users.noreply.github.com>
2019-01-17 15:34:57 +0200

    Complete 1.3

--- osa1/reactin-alkeet/src/index.js
+++ osa1/reactin-alkeet/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
+    const part1 = {
+      name: 'Reactin perusteet',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Tiedonvälitys propseilla',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'Komponenttien tila',
+      exercises: 14
+    }
 
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Content part1={part1} part2={part2} part3={part3} />
+            <Total part1={part1} part2={part2} part3={part3} />
         </div>
     )
 }
@@ -28,9 +34,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part={props.part1} exercises={props.exercises1} />
-            <Part part={props.part2} exercises={props.exercises2} />
-            <Part part={props.part3} exercises={props.exercises3} />
+            <Part part={props.part1} />
+            <Part part={props.part2} />
+            <Part part={props.part3} />
         </>
     )
 }
@@ -38,14 +44,14 @@ const Content = (props) => {
 const Part = (props) => {
     return (
         <p>
-            {props.part} {props.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     )
 }
 
 const Total = (props) => {
     return (
-        <p>yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
+        <p>yhteensä {props.part1.exercises + props.part2.exercises + props.part3.exercises} tehtävää</p>
     )
 }
 
