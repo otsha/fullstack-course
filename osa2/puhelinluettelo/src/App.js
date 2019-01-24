55e8e9f935c2b1b7f3407b5de19b2f3229cd67ae
otsha <32258613+otsha@users.noreply.github.com>
2019-01-24 13:47:51 +0200

    Complete 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,35 +2,46 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: ''}
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const addName = (event) => {
-        event.preventDefault();
+    const addPerson = (event) => {
+        event.preventDefault()
 
         if (newName === '') {
             window.alert('Name cannot be empty')
         } else if (persons.map(person => person.name).includes(newName)) {
             window.alert(`${newName} on jo luettelossa!`)
         } else {
-            setPersons(persons.concat({ name: newName }))
+            setPersons(persons.concat({ name: newName, number: newNumber }))
         }
     }
 
-    const handleInput = (event) => {
+    const handleNewName = (event) => {
         setNewName(event.target.value)
+    }
+
+    const handleNewNumber = (event) => {
+        setNewNumber(event.target.value)
     }
 
     return (
         <div>
             <h2>Puhelinluettelo</h2>
 
-            <form onSubmit={addName}>
+            <form onSubmit={addPerson}>
                 <div>
                     nimi: <input
                         value={newName}
-                        onChange={handleInput}
+                        onChange={handleNewName}
+                    />
+                </div>
+                <div>
+                    numero: <input
+                        value={newNumber}
+                        onChange={handleNewNumber}
                     />
                 </div>
                 <div>
@@ -39,7 +50,7 @@ const App = () => {
             </form>
 
             <h2>Numerot</h2>
-            {persons.map(person => <p key={person.name}>{person.name}</p>)}
+            {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
         </div>
     )
 
