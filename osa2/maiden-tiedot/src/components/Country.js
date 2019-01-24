848c0e757f576c9bb75313af091ff8473caa7abc
otsha <32258613+otsha@users.noreply.github.com>
2019-01-24 17:02:10 +0200

    Complete 2.14

--- osa2/maiden-tiedot/src/components/Country.js
+++ osa2/maiden-tiedot/src/components/Country.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Country = ({name, capital, population, languages, flag}) => {
+const Country = ({ name, capital, population, languages, flag }) => {
+    const [weather, setWeather] = useState([])
+
+    const weatherHook = () => {
+        axios.get(`https://api.apixu.com/v1/current.json?key=a89595b62ba749859c0144604192401&q=${capital}`)
+            .then(response => {
+                console.log(response.data.current)
+                setWeather(response.data.current)
+            })
+    }
+
+    useEffect(weatherHook, [])
+
     return (
         <div>
             <h1>{name}</h1>
@@ -10,7 +23,10 @@ const Country = ({name, capital, population, languages, flag}) => {
             <ul>
                 {languages.map(language => <li key={language.name}>{language.name}</li>)}
             </ul>
-            <img alt='flag' src={flag} width='10%' height='10%'/>
+            <img alt='flag' src={flag} width='10%' height='10%' />
+            <h2>Weather in {capital}</h2>
+            <p>temperature: {weather.temp_c} °C</p>
+            <p>wind: {weather.wind_kph} km/h</p>
         </div>
     )
 }
