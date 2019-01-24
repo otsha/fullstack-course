a2072cb6b66e2112e88168fd7e2404ae8d31a1c5
otsha <32258613+otsha@users.noreply.github.com>
2019-01-24 12:36:08 +0200

    Complete 2.4

--- osa2/reactin-alkeet-jatkuu/src/index.js
+++ osa2/reactin-alkeet-jatkuu/src/index.js
@@ -2,30 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 2,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Course course={course} />
+            {courses.map(course => <div key={course.id}><Course course={course} /></div>)}
         </div>
     )
 }
