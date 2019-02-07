b79a93a73ec5ebf467d1d3a8b84ea513a69e65c6
otsha <32258613+otsha@users.noreply.github.com>
2019-02-07 18:31:20 +0200

    Complete 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
@@ -14,6 +15,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
 
