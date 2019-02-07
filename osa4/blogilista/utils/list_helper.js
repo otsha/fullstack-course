1dedf43eb7652512fb6641ed285592842128bebf
otsha <32258613+otsha@users.noreply.github.com>
2019-02-07 12:03:00 +0200

    Complete 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,4 +10,22 @@ const totalLikes = (blogs) => {
     return blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    } else if (blogs.length === 1) {
+        return blogs[0]
+    } else {
+        let favourite = blogs[0]
+
+        for (let i = 0; i < blogs.length; i++) {
+            if (blogs[i].likes > favourite.likes) {
+                favourite = blogs[i]
+            }
+        }
+
+        return favourite
+    }
+}
+
+module.exports = { dummy, totalLikes, favouriteBlog }
