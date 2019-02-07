45eceba0408ef036d9d50cccba5ed7ecd59b852e
otsha <32258613+otsha@users.noreply.github.com>
2019-02-07 18:44:12 +0200

    Complete 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,19 +1,41 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getToken = request => {
+    const auth = request.get('authorization')
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+        return auth.substring(7)
+    }
+
+    return null
+}
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog
-    .find({}).populate('users', { username: 1, id: 1 })
+        .find({}).populate('users', { username: 1, id: 1 })
     response.json(blogs.map(b => b.toJSON()))
 })
 
 blogRouter.post('/', async (request, response, next) => {
-    const blog = new Blog(request.body)
-    const user = User.find({})[0]
-    blog.user = user;
+    const token = getToken(request)
 
     try {
+        const decoded = jwt.verify(token, process.env.SECRET)
+        if (!token || !decoded.id) {
+            return response.status(401).json({ error: "Invalid token" }).end()
+        }
+
+        const blog = new Blog(request.body)
+        const user = await User.findById(decoded.id)
+        blog.user = user;
+
         const result = await blog.save()
+
+        user.blogs = user.blogs.concat(result.id)
+        await user.save()
+
         response.status(201).json(result)
     } catch (exception) {
         response.status(400)
