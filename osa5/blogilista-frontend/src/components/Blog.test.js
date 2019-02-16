4bfa770003064dfdd5121f2598e20673ed5f62db
otsha <32258613+otsha@users.noreply.github.com>
2019-02-16 12:23:00 +0200

    Complete 5.14

--- osa5/blogilista-frontend/src/components/Blog.test.js
+++ osa5/blogilista-frontend/src/components/Blog.test.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
+const blog = {
+  title: 'testtitle',
+  author: 'testauthor',
+  likes: 5
+}
+
 describe('Simpleblog render tests', () => {
-  const blog = {
-    title: 'testtitle',
-    author: 'testauthor',
-    likes: 5
-  }
 
   test('blog title is rendered', () => {
     const component = render(<SimpleBlog blog={blog} />)
@@ -22,5 +23,17 @@ describe('Simpleblog render tests', () => {
   test('blog likes are rendered', () => {
     const component = render(<SimpleBlog blog={blog} />)
     expect(component.container).toHaveTextContent('5')
+  })
+})
+
+describe('Simpleblog button tests', () => {
+  const handler = jest.fn()
+
+  test('if like button is clicked twice the event handler is called twice', () => {
+    const { getByText } = render(<SimpleBlog blog={blog} onClick={handler} />)
+    const button = getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+    expect(handler.mock.calls.length).toBe(2)
   })
 })
