7cd4901667f126ad1a16fcc319a203a4602cb21e
otsha <32258613+otsha@users.noreply.github.com>
2019-02-22 12:10:07 +0200

    Complete 6.16

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecdoteService from './services/anecdoteService'
+import { initAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => props.initAnecdotes(anecdotes))
+  }, [])
 
   return (
     <div>
@@ -16,4 +22,8 @@ const App = () => {
   )
 }
 
-export default App
+const mapDispatchToProps = {
+  initAnecdotes
+}
+
+export default connect(null, mapDispatchToProps)(App)
---
+++ osa6/redux-anecdotes/src/services/anecdoteService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const res = await axios.get(baseUrl)
+    console.log(res.data)
+    return res.data
+}
+
+export default { getAll }
